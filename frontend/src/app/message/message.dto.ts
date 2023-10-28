6370c8b502d62749f52884feba7f19f0944555c7
kristjan.junus <kristjan.junus@helmes.com>
2023-10-28 17:46:11 +0300

    Remove frontend

--- frontend/src/app/message/message.dto.ts
+++
@@ -1,3 +0,0 @@
-export class MessageDto {
-  public body: string;
-}
--- frontend/src/app/web-socket/web-socket-service-factory.ts
+++
@@ -1,9 +0,0 @@
-import {webSocketConfig} from "./web-socket.config";
-import {WebSocketService} from "./web-socket.service";
-
-export function webSocketServiceFactory() {
-  const rxStomp = new WebSocketService();
-  rxStomp.configure(webSocketConfig);
-  rxStomp.activate();
-  return rxStomp;
-}
