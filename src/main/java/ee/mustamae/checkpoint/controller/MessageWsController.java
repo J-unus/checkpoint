41dcdec79c9f0fd029493fcad4a481beb1ec3fb7
kristjan.junus <kristjan.junus@helmes.com>
2023-12-11 16:38:13 +0200

    Remove POC code

--- src/main/java/ee/mustamae/checkpoint/controller/MessageWsController.java
+++ src/main/java/ee/mustamae/checkpoint/controller/MessageWsController.java
@@ -1,18 +1,16 @@
 package ee.mustamae.checkpoint.controller;
 
 import ee.mustamae.checkpoint.dto.MessageDto;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class MessageWsController {
 
   @MessageMapping("/checkpoint/{chatRoomUuid}")
   @SendTo("/topic/checkpoint/{chatRoomUuid}")
-  public MessageDto checkpoint(@DestinationVariable String chatRoomUuid, String message) {
-    return new MessageDto("Received, " + HtmlUtils.htmlEscape(message) + "!");
+  public MessageDto checkpoint(String message) {
+    return new MessageDto(message);
   }
 }
